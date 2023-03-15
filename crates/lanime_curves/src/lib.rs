ba95eb9b4b7e32716c748bcf9650a9ef70e30302
DasLixou <einlixou@gmail.com>
2023-03-15 13:02:53 +0100

    Naive way of implementing infinite n-size curves

--- crates/lanime_curves/src/lib.rs
+++ crates/lanime_curves/src/lib.rs
@@ -9,3 +9,52 @@ pub trait Curve {
     /// Interpolates a point on a Beziér Curve at `t` where `t` is `0..=1`
     fn interpolate(&self, t: f32) -> Point;
 }
+
+macro_rules! curve_math {
+    ($t:expr, {$($start:tt)+}, {$($end:tt)+}) => {
+        (1. - $t) * ($($start)+) + $t * ($($end)+)
+    };
+}
+
+#[rustfmt::skip]
+macro_rules! recursive_tool {
+    ($me:expr, $t:expr, $start_idx:expr, 1) => {
+        $me[$start_idx]
+    };
+    ($me:expr, $t:expr, $start_idx:expr, 2) => {
+        curve_math!($t, 
+            { recursive_tool!($me, $t, $start_idx, 1) },
+            { recursive_tool!($me, $t, $start_idx + 1, 1)}
+        )
+    };
+    ($me:expr, $t:expr, $start_idx:expr, 3) => {
+        curve_math!($t, 
+            { recursive_tool!($me, $t, $start_idx, 2) },
+            { recursive_tool!($me, $t, $start_idx + 1, 2)}
+        )
+    };
+    /*($me:expr, $t:expr, $start_idx:expr, $len:expr) => {
+        curve_math!(t, 
+            { recursive_tool!($me, $t, $start_idx, $len - 1) },
+            { recursive_tool!($me, $t, $start_idx + 1, $len - 1)}
+        )
+    };*/
+}
+
+impl Curve for &[Point; 1] {
+    fn interpolate(&self, _t: f32) -> Point {
+        self[0]
+    }
+}
+
+impl Curve for &[Point; 2] {
+    fn interpolate(&self, t: f32) -> Point {
+        curve_math!(t, { self[0] }, { self[1] })
+    }
+}
+
+impl Curve for &[Point; 3] {
+    fn interpolate(&self, t: f32) -> Point {
+        recursive_tool!(self, t, 0, 3)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,16 @@ fn main() {
         end: Point { x: 2., y: 0. },
     };
 
+    #[rustfmt::skip]
+    let linear = &[
+        Point { x: 0.0, y: 2.0 },
+        Point { x: 0.0, y: 0.0 },
+        Point { x: 2.0, y: 0.0 },
+    ];
+
     let mut t = 0.;
     while t <= 1.0 {
-        println!("{:?}", linear.interpolate(t));
+        println!("{:?}", Curve::interpolate(&linear, t));
         t += 0.1;
     }
     //pollster::block_on(lanime_renderer::run());
