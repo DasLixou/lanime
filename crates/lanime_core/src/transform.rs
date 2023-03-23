bb00185a8304e5446a68ecb7be2bb5727f5da77b
DasLixou <einlixou@gmail.com>
2023-03-23 15:54:41 +0100

    Some other shit

--- crates/lanime_core/src/transform.rs
+++ crates/lanime_core/src/transform.rs
@@ -11,18 +11,35 @@ impl Transform {
         origin: OriginPoint::DEFAULT,
         position: Position::DEFAULT,
     };
+
+    pub fn calculated_transform(&self) -> Position {
+        let offset = self.origin.offset(12., 7.);
+        Position {
+            x: self.position.x - offset.0,
+            y: self.position.y - offset.1,
+            z: self.position.z,
+        }
+    }
 }
 
 #[derive(Debug)]
 pub enum OriginPoint {
     TopLeft,
     Center,
-    CustomRelative(i32, i32),
-    CustomPercentage(f32, f32),
+    CustomOffset(fn(f32, f32) -> (f32, f32)),
 }
 
 impl OriginPoint {
     pub const DEFAULT: Self = Self::Center;
+
+    #[inline]
+    pub fn offset(&self, width: f32, height: f32) -> (f32, f32) {
+        match self {
+            OriginPoint::TopLeft => (0.0, 0.0),
+            OriginPoint::Center => (width / 2., height / 2.),
+            OriginPoint::CustomOffset(f) => f(width, height),
+        }
+    }
 }
 
 #[derive(Debug, BindFields)]
